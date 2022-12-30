c555d46900eb2c08eefeed060083ba6cefca902b
Rivdnb <rivio101@gmail.com>
2022-12-30 22:40:52 +0100

    fixed looking.cs

--- Assets/looking.cs
+++ Assets/looking.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Platformer.Core;
+using Platformer.Model;
 using UnityEngine;
 using Cinemachine;
+
+namespace Platformer.Gameplay
+{
 
     public class looking : MonoBehaviour
 
     {
-        public GameObject player;
-        
+        //public GameObject player;
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
         // Start is called before the first frame update
         void Start()
         {
@@ -17,15 +22,19 @@ using Cinemachine;
         // Update is called once per frame
         void Update()
         {
-            var vcam = GetComponent<CinemachineVirtualCamera>();
-            if(Input.GetAxis("Vertical") < -0.5){
-                vcam.Follow = player.transform.GetChild(0);
-            }
-            else if(Input.GetAxis("Vertical") > 0.5){
-                vcam.Follow = player.transform.GetChild(1);
-            }
-            else{
-                vcam.Follow = player.transform;
+            var player = model.player;
+            if(player.health.IsAlive){
+                var vcam = GetComponent<CinemachineVirtualCamera>();
+                if(Input.GetAxis("Vertical") < -0.5){
+                    vcam.Follow = player.transform.GetChild(0);
+                }
+                else if(Input.GetAxis("Vertical") > 0.5){
+                    vcam.Follow = player.transform.GetChild(1);
+                }
+                else{
+                    vcam.Follow = player.transform;
+                }
             }
         }
     }
+}
